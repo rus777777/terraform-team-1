bbc6f408a8a1c2008ccb88f98e8656f0c6d345c4
rus777777 <rbudaichiev@gmail.com>
2022-06-06 23:41:46 +0000

    delete spaces

--- asg/main.tf
+++ asg/main.tf
@@ -148,22 +148,6 @@ resource "aws_security_group" "app" {
     security_groups = [aws_security_group.elb.id]
   }
 
-  #   ingress {
-  #     description      = "http from Internet"
-  #     from_port        = 80
-  #     to_port          = 80
-  #     protocol         = "tcp"
-  #     cidr_blocks      = ["0.0.0.0/0"]
-  #   }
-
-  #   ingress {
-  #     description      = "http from Internet"
-  #     from_port        = 22
-  #     to_port          = 22
-  #     protocol         = "tcp"
-  #     cidr_blocks      = ["0.0.0.0/0"]
-  #   } 
-
   egress {
     from_port        = 0
     to_port          = 0
