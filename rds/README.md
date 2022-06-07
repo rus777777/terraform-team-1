99df91c990791e7359ad3be75f23195ae305f2f7
rus777777 <rbudaichiev@gmail.com>
2022-06-07 16:10:34 +0000

    added backend in Readme

--- rds/README.md
+++ rds/README.md
@@ -1,9 +1,9 @@
-# Terraform project - Team-1 - VPC
+# Terraform project - Team-1 - RDS
 
 For team work:
 
 1. create S3 bucket "terraform-tfstate-<Account_ID>" in region "us-east-1"
-2. create DynamoDB table "terraform-state-project1-vpc" with LockID key
+2. create DynamoDB table "terraform-state-project1-rds" with LockID key
 
 Add backend settings to backend.tf:
 ```
@@ -13,9 +13,9 @@ terraform {
     # terraform-tfstate-<Account_ID>
     bucket         = "terraform-tfstate-XXXXYYYYZZZZ" 
 
-    key            = "project-team-1/dev/vpc"
+    key            = "project-team-1/dev/rds"
     region         = "us-east-1"
-    dynamodb_table = "terraform-state-project1-vpc"
+    dynamodb_table = "terraform-state-project1-rds"
     encrypt        = true
   }
 }
--- rds/output.tf
+++ rds/output.tf
@@ -1,22 +1,28 @@
 
-# output "endpoint-address" {
-#   # Your code goes here 
-#   value = aws_db_instance.default.address
-# }
-# output "availability_zone" {
-#   # Your code goes here 
-#   value = aws_db_instance.default.availability_zone
-# }
-# output "engine" {
-#   # Your code goes here 
-#   value = aws_db_instance.default.engine
-# }
-# output "id" {
-#   # Your code goes here 
-#   value = aws_db_instance.default.id
-# }
-
-# output "port" {
-#   # Your code goes here 
-#   value = aws_db_instance.default.port
-# }
+output "address" {
+  # Your code goes here 
+  value = aws_db_instance.this.address
+}
+output "availability_zone" {
+  # Your code goes here 
+  value = aws_db_instance.this.availability_zone
+}
+output "engine" {
+  # Your code goes here 
+  value = aws_db_instance.this.engine
+}
+output "id" {
+  # Your code goes here 
+  value = aws_db_instance.this.id
+}
+output "port" {
+  # Your code goes here 
+  value = aws_db_instance.this.port
+}
+output "name" {
+  # Your code goes here 
+  value = aws_db_instance.this.name
+}
+output username {
+    value = var.username
+}
